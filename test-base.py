1c071b7c2c6d13218c0db7440f483232da68ebb1
xiao <153143828@qq.com>
2018-10-24 23:43:33 +0800

    test-base.py

--- test-base.py
+++ test-base.py
@@ -1,24 +1,15 @@
-
-import pymysql
-
-#class shuju():
+"""
+通讯录程序
+实现一个简单的通讯录，包含增删改查
+"""
 record_list=[]
 id=0
 def input_record():
     name=input("请输入姓名：")
     phone_number=input('请输入电话号码：')
-<<<<<<< HEAD
     record={'name':name,'phone_number':phone_number}
-
     return record
 
-#添加
-=======
-    record={'name'=name,'phone_number'=phone_number}
-
-    return record
-
->>>>>>> remotes/orj/master
 def add_record():
     record=input_record()
     global id
@@ -27,21 +18,18 @@ def add_record():
     record_list.append(record)
     return '添加成功'
 
-<<<<<<< HEAD
 #查询
-=======
->>>>>>> remotes/orj/master
+
 def query_record(name):
     query_result=[]
     query_ids=[]
     for record in record_list:
         if record['name']==name:
-<<<<<<< HEAD
             query_ids.append(record["id"])
             query_result.append(record)
     return query_ids,query_result
 
-#
+#删除
 def delete_record(name):
     query_ids,query_result=query_record(name)
     if len(query_ids)==0:
@@ -69,7 +57,7 @@ def delete_record(name):
                             pass
                     else:
                         print('输入错误！')
-
+#修改
 def change_record(name):
     query_ids,query_result= query_record(name)
     if len(query_ids)==0:
@@ -94,67 +82,31 @@ def change_record(name):
             query_result[0]['phone_number']=phone_number
             print('修改成功')
 
-        if __name__=='__main__':
-            while True:
-                menu="""通讯录：1.添加，2.查找，3.删除，4.修改，5.退出"""
-                print(menu)
-                s=input('请选择操作：')
-                if s in ['1','2','3','4','5']:
-                    if s=='1':
-                        msg=add_record()
-                        print(msg)
-                    if s=='2':
-                        name=input('请输入姓名：')
-                        query_ids,query_result=query_record(name)
-                        if len(query_ids)==0:
-                            print('不存在')
+    if __name__=='__main__':
+        while True:
+            menu="""通讯录：1.添加，2.查找，3.删除，4.修改，5.退出"""
+            print(menu)
+            s=input('请选择操作：')
+            if s in ['1','2','3','4','5']:
+            if s=='1':
+                    msg=add_record()
+                    print(msg)
+                if s=='2':
+                    name=input('请输入姓名：')
+                    query_ids,query_result=query_record(name)
+                    if len(query_ids)==0:
+                        print('不存在')
 
-                    if s=='3':
-                        name=input('请输入姓名：')
-                        delete_record(name)
+                if s=='3':
+                    name=input('请输入姓名：')
+                    delete_record(name)
 
-                    if s=='4':
-                        name=input('请输入姓名')
-                        change_record(name)
+                if s=='4':
+                    name=input('请输入姓名')
+                    change_record(name)
+                if s=='5':
+                    break
 
-                    if s=='5':
-                        break
-
-                else:
-                    print('输入有误！')
-                    continue
-=======
-            query_ids.append(record["record_id"])
-            query_result.append(record)
-    return query_ids,query_result
-
-def delete_record(name):
-    query_ids,query_result=query_record(name)
-
-def delete_record(name):
-    query_ids,query_result= query_record(name)
->>>>>>> remotes/orj/master
-
-
-
-
-
-
-
-<<<<<<< HEAD
-=======
-
-    def query_record():
-
-        listtable = [{'姓名'：'张三'，'电话': '12465', 'id': '1'}]
-
-
-
-
-        def change_record():
-
-
-    def delete_record():
-
-
->>>>>>> remotes/orj/master
+            else:
+                print('输入有误！')
+                continue
